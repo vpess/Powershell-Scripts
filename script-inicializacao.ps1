88a0d325b453b633783b24fa47de741a546de629
Pessanha <47800089+vpessanha@users.noreply.github.com>
2020-12-14 21:48:32 +0000

    Add files via upload

---
+++ script-inicializacao.ps1
@@ -0,0 +1,28 @@
+﻿Write-host "Lá vamos nós para mais um dia..."
+
+start-process iexplore.exe https://portaldoatendimento.petrobras.biz
+Start-Sleep -Seconds 3
+Start-process SnippingTool
+Start-Sleep -Seconds 3
+Start-Process outlook
+Start-Sleep -Seconds 3
+Start-Process firefox
+Start-Sleep -Seconds 3
+& 'C:\Lotus\Notes\NotesLocal.exe'
+Start-Sleep -Seconds 3
+Start-Process powershell.exe
+Start-Sleep -Seconds 3
+& 'C:\Users\bjbd\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\apps do Chrome\Norber.lnk'
+Start-Sleep -Seconds 3
+& 'C:\Users\bjbd\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\apps do Chrome\Outlook (Indra).lnk'
+Start-Sleep -Seconds 3
+& 'C:\Users\bjbd\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Localizador de Pessoas.lnk'
+Start-Sleep -Seconds 3
+& 'C:\Users\bjbd\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Teams (Petrobras).lnk'
+Start-Sleep -Seconds 3
+& 'C:\Users\bjbd\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\EclipsePhone.lnk'
+Start-Sleep -Seconds 3
+Start-Process powershell.exe -Verb runAs
+Start-Sleep -Seconds 3
+& 'C:\Users\bjbd\Desktop\AD_Explorer.exe'
+Start-Sleep -Seconds 3
